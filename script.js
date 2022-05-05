59f0d52aee800cc06589872d659f01e0a920346e
Zehan Khan <zehan9211@gmail.com>
2022-05-05 22:59:43 +0530

    version-2

--- script.js
+++ script.js
@@ -1,91 +1,115 @@
 
 const [PLAYER_A, PLAYER_B] = ["player One","player Two"]
-var scoreOne  = document.querySelector(".score_1");
-var scoreTwo  = document.querySelector(".score_2");
-var p1Won = document.querySelector(".p1");
-var p2Won = document.querySelector(".p2")
-var score = document.querySelector(".score");
-var currentPlayerSpan = document.querySelector("#current_player");
+var scoreA  = document.querySelector(".score_1");
+var scoreB  = document.querySelector(".score_2");
+var pAwinCount = document.querySelector(".p1");
+var pBWinCount = document.querySelector(".p2")
+var round = document.querySelector(".round");
+var winningPlayerBlock = document.querySelector("#winning_player");
 var dect = document.querySelector(".dect");
-
-var btn  = document.querySelectorAll("button")
-
-var body = document.querySelector("body");
-let currentPlayer = PLAYER_A
-currentPlayerSpan.innerText = currentPlayer
-let gamePause = false
+var start = document.querySelector(".start")
 
 var winCount = { 
     [PLAYER_A]: 0,
     [PLAYER_B]:0
 }
+
+var score = {
+    [PLAYER_A]: 100,
+    [PLAYER_B]: 100
+}
 var count = 0
 
-body.addEventListener("keypress",(event)=>{
-    if(gamePause) return
-    if ( event.keyCode === 100 ) {
+start.addEventListener("click",playRound);
+
+async function playRound(){
+    startRound()
+
+    for ( let i = 0 ; score[PLAYER_A] > 0 && score[PLAYER_B] > 0 ; i++ ) {
+        await delay(200);
         let power = usePower();
-        detuctScore( power, scoreOne, PLAYER_A );
-        
-    } 
-    if ( event.keyCode === 107 ) {
-        let power = usePower();
-        detuctScore( power, scoreTwo, PLAYER_B );
+        if(i % 2 == 0){
+            detuctScore( power, scoreB, PLAYER_B );
+        } else {
+            detuctScore( power, scoreA, PLAYER_A );
+        }
     }
-});
+
+    endRound()
+    if(calculateGameWinner()) {
+        start.style.display = "none"
+    }
+}
+
 
 // random power
 var usePower = ()=>{
-    return Math.floor( (Math.random() * 50) + 1 );
+    return Math.floor( (Math.random() * 5) + 1 );
 }
 
 // detuct score
 function detuctScore( power, playerScore, player ) {
-    if(currentPlayer !== player) return
-    let newScore = playerScore.innerText - power;
-    playerScore.nextElementSibling.innerText = "-"+power;
-    playerScore.nextElementSibling.style.color = "red";
-    setTimeout( () => { 
-        playerScore.nextElementSibling.innerText = "";
-    },500)
-    if ( newScore >= 0 ){
-        playerScore.innerText  = newScore;
+    score[player] =  score[player] - power;
+    // playerScore.nextElementSibling.innerText = "-"+power;
+    // playerScore.nextElementSibling.style.color = "red";
+    // setTimeout( () => { 
+        // playerScore.nextElementSibling.innerText = "";
+    // },200)
+
+    if ( score[player] >= 0 ){
+        playerScore.innerText  = score[player];
     } else {
         playerScore.innerText  = 0;
-        document.querySelector(".result").innerText = `${player} Die`
-        winCount[player] += 1;
-        p1Won.innerText = winCount[PLAYER_B];
-        p2Won.innerText = winCount[PLAYER_A];
-        if ( winCount[PLAYER_A] >= 3 ) {
-            console.log(p1Won.innerText,"3")
-            document.querySelector(".break").innerText = "Player One Won By 3 ponit"
-            stopGame()
-            return
-        }
-        if ( winCount[PLAYER_B] >= "3" ) {
-            document.querySelector(".break").innerText = "Player Two Won By 3 ponit"
-            stopGame()
-            return
-        } else if ( p1Won.innerText < 3 || p2Won.innerText < 3 ) {
-            startGame();
-        }
+        calculateRoundWinner()
     }
-    toggleCurrentPlayer()
 }
 
-function startGame(){
+function calculateRoundWinner(){
+    if(score[PLAYER_A]<=0) {
+        winCount[PLAYER_B] += 1
+    }
+    if(score[PLAYER_B]<=0) {
+        winCount[PLAYER_A] += 1
+    }
+    setWinCount()
+}
+
+function calculateGameWinner() {
+    if(winCount[PLAYER_A] >=3) {
+        winningPlayerBlock.innerText = "Player One Win"
+        return true
+    } if(winCount[PLAYER_B] >=3) {
+        winningPlayerBlock.innerText = "Player Two Win"
+        return true
+    }
+    return false
+}
+
+function setWinCount(){
+    pAwinCount.innerText = winCount[PLAYER_A];
+    pBWinCount.innerText = winCount[PLAYER_B];
+}
+
+function setScore() {
+    scoreA.innerText = score[PLAYER_A]
+    scoreB.innerText = score[PLAYER_B]
+}
+
+function startRound(){
     count += 1;
-    score.innerText = count
-    if ( count <= 5 ) {
-        scoreOne.innerText = 100;
-        scoreTwo.innerText = 100;
-    } else {
-        score.innerText = "Game Over"
-    }
+    updateDOM()
 }
 
-function stopGame(){
-  gamePause = true
+function endRound() {
+    if ( count <= 5 ) {
+        score[PLAYER_A] = 100;
+        score[PLAYER_B] = 100;
+        updateDOM()
+    } else {
+        updateDOM()
+        round.innerText = "Game Over"
+    }
+    
 }
 
 function toggleCurrentPlayer(){
@@ -93,8 +117,13 @@ function toggleCurrentPlayer(){
     currentPlayerSpan.innerText = currentPlayer
 }
 
-// result
-// rounds
-// one chance for each player
 
-console.log("script is running");
+function updateDOM() {
+    round.innerText = count
+    setWinCount()
+    setScore()
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
